6cb4cb30c808d5345293c8d9b7fe2d608a4242dd
lightnest57 <lightnest57@gmail.com>
2023-11-27 19:19:15 +0000

    afterlife

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/citrus/ProjectBlaze-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls ProjectBlaze-*.zip)"
-      rclone copy ~/rom/out/target/product/citrus/ProjectBlaze-*.zip 298:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls ProjectBlaze-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/citrus/AfterLife-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls AfterLife-*.zip)"
+      rclone copy ~/rom/out/target/product/citrus/AfterLife-*.zip 298:citrus -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls AfterLife-*.zip) Uploaded Successfully!"
 fi
