718a41d7f96894e6bdfc6ec47514f884b9b624e0
lightnest57 <122135694+lightnest57@users.noreply.github.com>
2023-11-29 00:26:02 +0700

    gas

--- w.sh
+++ w.sh
@@ -2,11 +2,11 @@
 
 sudo apt update -y && sudo apt upgrade -y
 sudo apt -y install util-linux
-sudo fallocate -l 8G /swapfile
-sudo dd if=/dev/zero of=/swapfile bs=1024 count=8388608
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+mkdir -p ~/tmp
+sudo dd if=/dev/zero of=~/tmp/swapfile bs=1024 count=8388608
+sudo chmod 600 ~/tmp/swapfile
+sudo mkswap ~/tmp/swapfile
+sudo swapon ~/tmp/swapfile
 sudo swapon --show
 
 export CCACHE_DIR=~/ccache
