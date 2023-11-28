6378e3b7c37c629edc3817395d622137f40affc2
lightnest57 <122135694+lightnest57@users.noreply.github.com>
2023-11-29 02:23:02 +0700

    ccache 2

--- w.sh
+++ w.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
 sudo apt update -y && sudo apt upgrade -y
-sudo apt -y install util-linux
-mkdir -p ~/tmp
-sudo dd if=/dev/zero of=~/tmp/swapfile bs=1024 count=8388608
-sudo chmod 600 ~/tmp/swapfile
-sudo mkswap ~/tmp/swapfile
-sudo swapon ~/tmp/swapfile
-sudo swapon --show
-
 export CCACHE_DIR=~/ccache
 sleep 1m
 
