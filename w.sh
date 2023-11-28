62604d308e4627f0a4a95080a8f0b175a18c09c5
lightnest57 <122135694+lightnest57@users.noreply.github.com>
2023-11-28 23:57:43 +0700

    gas

--- w.sh
+++ w.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+sudo apt update -y && sudo apt upgrade -y
+sudo apt -y install util-linux
+sudo fallocate -l 8G /swapfile
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=8388608
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+
 export CCACHE_DIR=~/ccache
 sleep 1m
 
