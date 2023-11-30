225c14a688aba29ca5bd52490a436aa14c3ca0a6
lightnest57 <lightnest57@gmail.com>
2023-11-30 08:46:51 +0000

    qassa rebuild

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/citrus/evolution_citrus*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls evolution_citrus.zip)"
-      rclone copy ~/rom/out/target/product/citrus/evolution_citrus*.zip 298:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls evolution_citrus*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/juice/qassa_S*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls qassa_S*.zip)"
+      rclone copy ~/rom/out/target/product/juice/qassa_S*.zip fk09:juice -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls qassa_S*.zip) Uploaded Successfully!"
 fi
