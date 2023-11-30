e8394215973792f8205cc41dc32329399c09b07e
lightnest57 <lightnest57@gmail.com>
2023-11-30 11:46:04 +0000

    ccache 1x lagit

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 if [ -f ~/rom/out/target/product/citrus/AfterLife-*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls AfterLife-*.zip)"
-      rclone copy ~/rom/out/target/product/citrus/AfterLife-*.zip 298:citrus -P
+      rclone copy ~/rom/out/target/product/citrus/AfterLife-*.zip fk09:citrus -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls AfterLife-*.zip) Uploaded Successfully!"
 fi
