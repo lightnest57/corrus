4fb9c860e518af97e2a40ce2d59b83f26077127a
lightnest57 <lightnest57@gmail.com>
2023-12-24 01:39:15 +0000

    gass fluid

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/citrus/dotOS-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls evolution_citrus.zip)"
-      rclone copy ~/rom/out/target/product/citrus/dotOS-*.zip fk09:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls dotOS-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/chime/Fluid-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/chime/ && ls evolution_chime.zip)"
+      rclone copy ~/rom/out/target/product/chime/Fluid-*.zip fk09:chime -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/chime/ && ls Fluid-*.zip) Uploaded Successfully!"
 fi
