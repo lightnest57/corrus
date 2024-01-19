24f2c03d0a04699f25b281cbb13da310b581eed6
lightnest57 <lightnest57@gmail.com>
2024-01-19 17:51:10 +0000

    gass

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/citrus/Havoc-OS*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls Havoc-OS*.zip)"
-      rclone copy ~/rom/out/target/product/citrus/Havoc-OS*.zip fk09:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls Havoc-OS*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/citrus/Kaleidoscope-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls Kaleidoscope-*.zip)"
+      rclone copy ~/rom/out/target/product/citrus/Kaleidoscope-*.zip fk09:citrus -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls Kaleidoscope-*.zip) Uploaded Successfully!"
 fi
