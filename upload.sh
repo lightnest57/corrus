6e76b0ad28dd1ca82026199a5d2bac80104fa930
lightnest57 <lightnest57@gmail.com>
2024-01-26 01:29:07 +0000

    gass

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
-if [ -f ~/rom/out/target/product/citrus/TenX-OS*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls TenX-OS*.zip)"
-      rclone copy ~/rom/out/target/product/citrus/TenX-OS*.zip fk09:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls TenX-OS*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/citrus/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz ]; then
+      rclone copy ~/rom/out/target/product/citrus/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz fk09:citrus -P
+      rclone copy ~/rom/out/target/product/citrus/obj/KERNEL_OBJ/arch/arm64/boot/dtbo.img fk09:citrus -P
 fi
