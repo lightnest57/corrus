36090480c391dcfb4b1a21d904a4c0c0c6bf8c8f
lightnest57 <lightnest57@gmail.com>
2024-02-07 09:38:53 +0000

    rr jus

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/juice/qassa_S*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls qassa_S*.zip)"
-      rclone copy ~/rom/out/target/product/juice/qassa_S*.zip fk09:juice -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls qassa_S*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/juice/RR-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls RR-*.zip)"
+      rclone copy ~/rom/out/target/product/juice/RR-*.zip fk09:juice -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls RR-*.zip) Uploaded Successfully!"
 fi
