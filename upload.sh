98c525938025da1d487e293dfa9fcb2aef3153b0
lightnest57 <lightnest57@gmail.com>
2024-04-30 19:17:36 +0000

    kont

--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
-if [ -f ~/rom/out/target/product/chime/xdCLO_*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/chime/ && ls xdCLO_*.zip)"
-      rclone copy ~/rom/out/target/product/chime/xdCLO_*.zip fk09:chime -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/chime/ && ls xdCLO_*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/Itel_S666LN/vendor_boot.img ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/Itel_S666LN/ && ls xdCLO_*.img)"
+      rclone copy ~/rom/out/target/product/Itel_S666LN/vendor_boot.img fk09:Itel_S666LN -P      
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/Itel_S666LN/ && ls xdCLO_*.img) Uploaded Successfully!"
 fi
+rclone copy ~/rom/out/target/product/itel/vendor_boot.img fk09:Itel_S666LN -P
