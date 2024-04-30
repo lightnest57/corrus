0a6dca3afd0a55315552c32656c4bf0a7e73397d
lightnest57 <122135694+lightnest57@users.noreply.github.com>
2024-05-01 03:22:34 +0700

    terakhir su

--- upload.sh
+++ upload.sh
@@ -1,5 +1,3 @@
-if [ -f ~/rom/out/target/product/Itel-S666LN/vendor_boot.img ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/Itel_S666LN/ && ls xdCLO_*.img)"
-      rclone copy ~/rom/out/target/product/Itel-S666LN/vendor_boot.img fk09:Itel_S666LN -P      
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/Itel_S666LN/ && ls xdCLO_*.img) Uploaded Successfully!"
-fi]
+curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/Itel_S666LN/ && ls vendor_boot.img)"
+rclone copy ~/rom/out/target/product/Itel-S666LN/vendor_boot.img fk09:Itel_S666LN -P      
+curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/Itel_S666LN/ && ls vendor_boot.img) Uploaded Successfully!"
