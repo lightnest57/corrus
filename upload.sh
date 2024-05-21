ccb577492f028e7f5f04003e3136bc363cc1dbc2
lightnest57 <lightnest57@gmail.com>
2024-05-21 18:01:31 +0000

    gass

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/chime/Havoc-OS*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/chime/ && ls Havoc-OS*.zip)"
-      rclone copy ~/rom/out/target/product/chime/Havoc-OS*.zip fk09:chime -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/chime/ && ls Havoc-OS*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/mido/Havoc-OS*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/mido/ && ls Havoc-OS*.zip)"
+      rclone copy ~/rom/out/target/product/mido/Havoc-OS*.zip fk09:mido -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/mido/ && ls Havoc-OS*.zip) Uploaded Successfully!"
 fi
