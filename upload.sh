72e51a7bb92c45a2328d381bbafa0b27144022d2
lightnest57 <lightnest57@gmail.com>
2024-06-02 20:48:10 +0000

    COBA

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/mido/Havoc-OS*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/mido/ && ls Havoc-OS*.zip)"
-      rclone copy ~/rom/out/target/product/mido/Havoc-OS*.zip cirrus:mido -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/mido/ && ls Havoc-OS*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/mido/RR-P*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/mido/ && ls RR-P*.zip)"
+      rclone copy ~/rom/out/target/product/mido/RR-P*.zip cirrus:mido -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/mido/ && ls RR-P*.zip) Uploaded Successfully!"
 fi
